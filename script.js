381d7c1e85679538d02dbcb7a51e31c42305f429
Adriel Windows <adrielcostamartins608@gmail.com>
2026-02-18 22:55:55 -0300

    pontos principais finalizados

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var corpo = document.querySelector('body')
 var modclaro = document.getElementById('modo-claro')
 var modescuro = document.getElementById('modo-escuro')
+var caixa = document.getElementById('form1')
 
 function escuro(){
 
     modescuro.style.display = 'none'
     modclaro.style.display = 'block'
     corpo.style.background = '#2c2f33'
+    caixa.style.backgroundImage = "url(imagens/escuro.png)"
+    caixa.style.backgroundSize = 'cover'
+    caixa.style.backgroundPosition = 'center center'
 
 }
 
@@ -15,5 +19,8 @@ function claro(){
     modclaro.style.display = 'none'
     modescuro.style.display = 'block'
     corpo.style.background = '#dee1e4'
+    caixa.style.backgroundImage = "url(imagens/claro.png)"
+    caixa.style.backgroundSize = 'cover'
+    caixa.style.backgroundPosition = 'center center'
 
 }
