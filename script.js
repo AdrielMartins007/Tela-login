4c7a805642cd8c669f61eccbd92dc70bf7ac9135
Adriel Windows <adrielcostamartins608@gmail.com>
2026-02-18 22:33:49 -0300

    criação do modo escuro e claro

---
+++ script.js
@@ -0,0 +1,15 @@
+var corpo = document.querySelector('body')
+var modclaro = document.getElementById('modo-claro')
+var modescuro = document.getElementById('modo-escuro')
+
+function escuro(){
+    modescuro.style.display = 'none'
+    modclaro.style.display = 'block'
+    corpo.style.background = '#dee1e4'
+}
+
+function claro(){
+    modclaro.style.display = 'none'
+    modescuro.style.display = 'block'
+    corpo.style.background = '#2c2f33'
+}
