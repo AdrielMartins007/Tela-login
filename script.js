2dade8abbddf6bdca3237bed6edecd537bee19e0
Adriel Windows <adrielcostamartins608@gmail.com>
2026-02-18 22:35:45 -0300

    ajustes

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ var modclaro = document.getElementById('modo-claro')
 var modescuro = document.getElementById('modo-escuro')
 
 function escuro(){
+
     modescuro.style.display = 'none'
     modclaro.style.display = 'block'
-    corpo.style.background = '#dee1e4'
+    corpo.style.background = '#2c2f33'
+
 }
 
 function claro(){
+
     modclaro.style.display = 'none'
     modescuro.style.display = 'block'
-    corpo.style.background = '#2c2f33'
+    corpo.style.background = '#dee1e4'
+
 }
